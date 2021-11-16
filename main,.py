75cd171e7da59f5364586cf10c6d395c6296ee64
KingDrake1 <94483574+KingDrake1@users.noreply.github.com>
2021-11-16 14:31:51 -0500

    main

---
+++ main,.py
@@ -0,0 +1,7 @@
+print("Hello Nicholas")
+yon = input(print("Do you like cheese, nicholas?"))
+if yon == "y":
+    print("Cheese is very good, i am glad you like cheese")
+
+if yon == "n":
+    print("Eat more cheese nicholas")
